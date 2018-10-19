32ac1f4685688857a52a8c806164e80753917910
washwash <washingweb@hotmail.com>
2018-10-19 22:40:04 +0800

    add hdwallet infura deploy

--- truffle.js
+++ truffle.js
@@ -1,13 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-    networks: {
-        development: {
-                  host: "127.0.0.1",
-                  port: 8545,
-                  network_id: "*" // Match any network id
-
-        }
-
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/ab6a3cb9292c43c38caa7507da6de9c5"
+        );
+      },
+      network_id: "3",
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
+  }
 };
